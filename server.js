6822b106c044d0490a30a83cc0b20c3b27f46db5
Your Name <ballagayatri18@gmail.com>
2026-06-28 13:11:30 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -3,22 +3,16 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-// connectDB disabled temporarily
-// const connectDB = require("./config/db");
-// connectDB();
-
 const app = express();
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Library Management API is running");
 });
 
-// Comment API routes that use database
-// app.use("/api/auth", require("./routes/authRoutes"));
-// app.use("/api/books", require("./routes/bookRoutes"));
-// app.use("/api/members", require("./routes/memberRoutes"));
+const PORT = process.env.PORT || 5000;
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen(PORT, () => {
   console.log("Server started");
 });
