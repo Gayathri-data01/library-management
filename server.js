2841118d1090e2d081ff56d7382d42876886dcc6
Your Name <ballagayatri18@gmail.com>
2026-06-28 13:04:20 +0530

    Disable DB and routes temporarily

--- server.js
+++ server.js
@@ -2,19 +2,23 @@ const express = require("express");
 const dotenv = require("dotenv");
 
 dotenv.config();
-connectDB();
+
+// connectDB disabled temporarily
+// const connectDB = require("./config/db");
+// connectDB();
 
 const app = express();
 app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Library Management API is running");
 });
 
-app.use("/api/auth", require("./routes/authRoutes"));
-app.use("/api/books", require("./routes/bookRoutes"));
-app.use("/api/members", require("./routes/memberRoutes"));
+// Comment API routes that use database
+// app.use("/api/auth", require("./routes/authRoutes"));
+// app.use("/api/books", require("./routes/bookRoutes"));
+// app.use("/api/members", require("./routes/memberRoutes"));
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log("Server started");
 });
-
